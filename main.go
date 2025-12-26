87e184213b99a4196e4097689e045a9b3677dc43
Miguel Amorim <miguelsantosamorim3@gmail.com>
2025-12-26 14:54:24 -0300

    v2: adicionado funcao e loop

--- main.go
+++ main.go
@@ -1,48 +1,62 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"os"
 	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
+
+// variavel global
+var valorSaque float64
+
+func digiteSaqueValor() {
+	fmt.Print("digite o valor que quer sacar: ")
+	fmt.Scanln(&valorSaque)
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("=== CAIXA ELETRONICO v1 ===") // isso é falso e não usa dinheiro real, blz?
-	fmt.Print("digite seu nome: ")
-	var nome string
+	for {
+		fmt.Println("=== CAIXA ELETRONICO v2 ===") // isso é falso e não usa dinheiro real, blz?
 
-	// pega o nome completo
-	nome, _ = reader.ReadString('\n')
-	nome = strings.TrimSpace(nome)
+		fmt.Println(" '/exit' para sair ")
 
-	fmt.Print("digite seu saldo atual: ")
-	var saldoAtual float64
-	fmt.Scanln(&saldoAtual)
+		fmt.Print("digite seu nome: ")
+		var nome string
 
-	if saldoAtual < 0 {
-		fmt.Println("erro no programa: o valor digitado é invalido")
-		return
-	}
+		// pega o nome completo
+		nome, _ = reader.ReadString('\n')
+		nome = strings.TrimSpace(nome)
 
-	// no futuro espero este bloco de codigo vai virar uma função
-	fmt.Print("digite o valor que quer sacar: ")
-	var valorSaque float64
-	fmt.Scanln(&valorSaque)
+		if nome == "/exit" {
+			break
+		}
 
-	// validação de entrada
-	if valorSaque <= 0 {
-		fmt.Println("erro no programa: o valor digitado é invalido")
-		return
-	} else if valorSaque > saldoAtual {
-		fmt.Println("erro no programa: saque digitado maior que seu saldo atual")
-		return
-	} else {
-		fmt.Println("saldo autorizado")
-		saldoAtual -= valorSaque
-		fmt.Printf("saldo atual:  %.2f\n", saldoAtual)
-		fmt.Printf("valor do saque: %.2f\n", valorSaque)
+		fmt.Print("digite seu saldo atual: ")
+		var saldoAtual float64
+		fmt.Scanln(&saldoAtual)
+
+		if saldoAtual < 0 {
+			fmt.Println("erro no programa: o valor digitado é invalido")
+			return
+		}
+
+		digiteSaqueValor()
+
+		// validação de entrada
+		if valorSaque <= 0 {
+			fmt.Println("erro no programa: o valor digitado é invalido")
+			return
+		} else if valorSaque > saldoAtual {
+			fmt.Println("erro no programa: saque digitado maior que seu saldo atual")
+			return
+		} else {
+			fmt.Println("saldo autorizado")
+			saldoAtual -= valorSaque
+			fmt.Printf("saldo atual:  %.2f\n", saldoAtual)
+			fmt.Printf("valor do saque: %.2f\n", valorSaque)
+		}
 	}
 }
